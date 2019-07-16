b6b265d3c72a9773f9e3dc2156323427c6b68a00
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2019-07-16 01:53:43 +0000

    Convocatorio Nacional Funcionando

--- recolector.py
+++ recolector.py
@@ -1,4 +1,6 @@
 import requests
+import lxml.etree
+import lxml.html
 import re
 import math
 from datetime import datetime as dt, timezone
@@ -17,8 +19,9 @@ headers = {
         #'Connection': 'keep-alive',
         }
 
-def getTK():
-    return requests.get('https://www.sicoes.gob.bo/lib/download/tk.php', headers=headers, cookies=cookies)
+def getTK(sesion):
+    ret = sesion.get('https://www.sicoes.gob.bo/lib/download/tk.php')
+    return ret
 
 def proord(A, c, s, w, x): 
     i = len(A)
@@ -57,64 +60,117 @@ def proord(A, c, s, w, x):
             A[t] = z[(t - 1)]
     return A
 
-def botcolobt(operacion, tipo, timehour):
+def botcolobt(operacion, tipo, timehour, sesion):
+    captcha = getTK(sesion=sesion)
     data = {
-            'opetpo': operacion, # 'convNacional',
-            'tpoope': tipo,      # 'Simple',
+            'opetpo': operacion, # 'convNacional','convInternacional','contResueltos','desistimientoCont'
+            'tpoope': tipo,      # 'Simple','Avanzada'
             'time': timehour
             }
-    return requests.post('https://www.sicoes.gob.bo/portal/contrataciones/operacion2.php', headers=headers, cookies=cookies, data=data)
+    ret =  sesion.post('https://www.sicoes.gob.bo/portal/contrataciones/operacion2.php', data=data)
+    return ( ret, captcha )
 
-def getRutaColumnasContrataciones(operacion, tipo):
+def getRutaColumnasContrataciones(operacion, tipo, sesion):
     ruta = "/portal/contrataciones/operacion.php"
-    rpto = botcolobt(operacion, tipo, int(dt.now().replace(tzinfo=timezone.utc).timestamp()))
-    columnastmp = rpto.json()['sectorsec']
-    columnas = proord(columnastmp, rpto.json()['a'], rpto.json()['b'], rpto.json()['fx'], rpto.json()['fy'])
-    return [ruta, columnas]
+    rpto = botcolobt(operacion, tipo, int(dt.now().replace(tzinfo=timezone.utc).timestamp()), sesion=sesion )
+    columnastmp = rpto[0].json()['sectorsec']
+    columnas = proord(columnastmp, rpto[0].json()['a'], rpto[0].json()['b'], rpto[0].json()['fx'], rpto[0].json()['fy'])
+    return [ruta, columnas, rpto[1]]
 
 def getRutaColumnasADJ(operacion, tipo):
     return getRutaColumnasContrataciones(operacion, tipo)
 
-def getDatos(operacion, tipo, columnas, ruta, pag ):
-    captcha = getTK()
+def getDatos(operacion, tipo, columnas, ruta, pag, captcha, sesion, b9 ):
     c = re.compile(r"ntv\s*=\s*'([a-zA-Z0-9]+)'\s*;vtv\s*=\s'([a-zA-Z0-9]+)'\s*;")
     d = re.findall(c, captcha.text)[0]
     e = re.compile(r"%([A-F0-9]{2})")
     data = {
-            'entidad': '',
-            'objetoContrato': '',
-            'publicacionDesde': '',
-            'publicacionHasta': '',
-            'presentacionPropuestasDesde': '',
-            'presentacionPropuestasHasta': '',
+            'arpcDespliegue': 'option3',
+            'B903A6B7': b9,
+            'captcha': '',
+            'codigoContrato': '',
+            'codigoDpto': '',
+            'codigoModalidad': '',
+            'codigoNormativa': '',
+            'costoPlieDespliegue': 'option3',
             'cuce1': '',
             'cuce2': '',
             'cuce3': '',
             'cuce4': '',
             'cuce5': '',
             'cuce6': '',
-            'r1': '11',
-            'bienes': 'B',
-            'obras': 'O',
-            'servicios': 'S',
-            'consultoria': 'C',
-            'B903A6B7': '71FD0BFAC3F2E7277707F9191D46CF71',
-            'operacion': operacion,
-            'nroRegistros': '50',
+            'desiertaDesde': '',
+            'desiertaHasta': '',
+            'dptoDespliegue': 'option3',
             'draw': str(pag),
+            'entidad': '',
+            'fechaAdjudicacionDespliegue': 'option2',
+            'fechaReunionDespliegue': 'option1',
+            'length': '10',
+            'montoDesde': '',
+            'montoHasta': '',
+            'nomtoGarDespliegue': 'option2',
+            'normativaDespliegue': 'option3',
+            'nroContrato': '',
+            'nroRegistros': '10',
+            'objetoContrato': '',
+            'operacion': operacion,
+            'personaContDespliegue': 'on',
+            'presentacionPropuestasDesde': '',
+            'presentacionPropuestasHasta': '',
+            'publicacionDesde': '',
+            'publicacionHasta': '',
+            'r1': '11',
             'start': '0',
-            'length': '50',
-            'captcha': '',
+            #'bienes': 'B',
+            #'obras': 'O',
+            #'servicios': 'S',
+            #'consultoria': 'C',
             d[0]: d[1]
             }
-    res = requests.post('https://www.sicoes.gob.bo' + ruta, headers=headers, cookies=cookies, data=data).json()
+    res = sesion.post('https://www.sicoes.gob.bo' + ruta, data=data).json()
     r1 = []
-    for a in res.get('data'):
+    try:
+      for a in res.get('data'):
         c=[]
         for b in columnas:
             c.append( bytes.fromhex( ''.join(re.findall(e, a[b] ) ) ).decode('ISO_8859-1') )
         r1.append(c)
-    return r1
+    except:
+        print(res)
+    return {'datos':r1,'recordsTotal':res.get('recordsTotal')}
 
 # r = getRutaColumnasContrataciones('convNacional', 'Simple')
 #getDatos(columnas=r[1],ruta=r[0],operacion='convNacional',tipo='Simple')
+
+
+def convNacional():
+    tipo = 'Avanzada'
+    s = requests.Session()
+    s.headers.update({'Referer': 'https://www.sicoes.gob.bo/portal/contrataciones/busqueda/convocatorias.php?tipo=convNacional',
+                      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'})
+    index = s.get("https://www.sicoes.gob.bo/portal/contrataciones/busqueda/convocatorias.php?tipo=convNacional")
+    b52=re.compile(r'name="B903A6B7" value="([0-9A-F]+)"')
+    b53 = b52.findall(index.text)
+    if len(b53) < 4:
+        index = s.get("https://www.sicoes.gob.bo/portal/contrataciones/busqueda/convocatorias.php?tipo=convNacional")
+    b53 = b52.findall(index.text)
+    r = getRutaColumnasContrataciones(operacion='convNacional', tipo=tipo, sesion=s)
+    total = 1
+    pag = 1
+    while(pag<=total):
+      r = getRutaColumnasContrataciones(operacion='convNacional', tipo=tipo, sesion=s)
+      datos = getDatos(columnas=r[1],ruta=r[0],operacion='convNacional',tipo=tipo, sesion=s, captcha=r[2], pag=pag, b9=b53[2])
+      total = int(datos['recordsTotal'])/10
+      pag = pag + 1
+      #index = s.get("https://www.sicoes.gob.bo/portal/contrataciones/busqueda/convocatorias.php?tipo=convNacional")
+      #b52=re.compile(r'name="B903A6B7" value="([0-9A-F]+)"')
+      #b53 = b52.findall(index.text)
+      #b53 = [b53[2]]
+      #print(b53)
+ 
+      print(datos)
+      print("Pagina")
+      print(pag)
+      print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+
